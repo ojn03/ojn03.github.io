d1bf4b36d105738d2c12f83c070a621d8959b305
OJisMe <olivierjohnnn@gmail.com>
2023-08-27 21:12:16 -0400

    added experiences +  responsiveness

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,83 +1,97 @@
-import React from 'react'
-import me from 'public/me.jpg'
-import Image from 'next/image'
-import Experiences from '@/components/Experiences'
-import Count from '@/components/Counter'
-import scrolldown from 'public/scrolldown.gif'
+import React from "react";
+import me from "public/me.jpg";
+import Image from "next/image";
+import Experiences from "@/components/Experiences";
+import Count from "@/components/Counter";
+import scrolldown from "public/scrolldown.gif";
 export const metadata = {
-  title: 'Olivier-John Nzia | About',
-}
-//todo scroll down arrow
+	title: "Olivier-John Nzia | About"
+};
 const page = () => {
-  return (
+	return (
+		<main className="flex w-full flex-col items-center justify-center">
+			{/* layout */}
+			<div className="h-full inline-block z-0 pt-8 sm:p-16 lg:p-32 ">
+				<h1 className="mb-16 text-center">Rome wasn&apos;t built in a day</h1>
+				<div className="relative grid w-fit h-fit grid-cols-8 gap-16">
+					{/* Image */}
 
-    <main className=' flex w-full flex-col items-center justify-center'>
-      {/* layout */}
-      <div className='h-full inline-block z-0 p-32 pt-16'>
-        <h1 className='mb-16 text-center'>Rome wasn&apos;t built in a day</h1>
-        <div className='relative grid w-fit h-fit grid-cols-8 gap-16'>
-          {/* <div className=''>
+					<div className="col-start-2 col-span-6 md:order-2 xl:col-span-3 md:col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark p-8 bg-light ">
+						<div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-3xl bg-dark" />
+						<Image
+							src={me}
+							alt="me.jpg"
+							className="w-full h-auto rounded-2xl"
+						/>
+					</div>
 
-            </div> */}
-          <div className=' col-span-3 flex gap-4 flex-col items-start justify-start '>
-            <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Bio</h2>
-            <p className='font-medium '>
-              My name is <b>Olivier John Nzia</b> and I&apos;m a software engineer. I&apos;m pursuing my bachelors in Computer Science (with a software concentration) at <b>Northeastern University</b>.
-            </p>
-            <p className='font-medium'>
-              I love to build things and I do so with a passion. I believe anything worthwile requires <b>dedication, effort, and time</b>, because Rome wasn&apos;t built in a day. Plan, Build, Test, and when you&apos;re done, <b>iterate and repeat</b>.
-            </p>
+					{/* BIO */}
+					<div className="col-span-8 md:order-1 xl:col-span-3 md:col-span-4 flex gap-4 flex-col items-start justify-start ">
+						<h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+							Biography
+						</h2>
+						<p className="font-medium ">
+							My name is <b>Olivier John Nzia</b> and I&apos;m a software
+							engineer. I&apos;m pursuing my bachelors in Computer Science (with
+							a software concentration) at <b>Northeastern University</b>.
+						</p>
+						<p className="font-medium">
+							I love to build things and I do so with a passion. I believe
+							anything worthwile requires <b>dedication, effort, and time</b>,
+							because Rome wasn&apos;t built in a day. Plan, Build, Test, and
+							when you&apos;re done, <b>iterate and repeat</b>.
+						</p>
 
-            <p className='font-medium'>
-              I&apos;m currently looking for a full-time co-op/internship for the Fall 2023 semester so if you&apos;re interested in working with me, feel free to reach out!
-            </p>
+						<p className="font-medium">
+							I&apos;m currently looking for a full-time co-op/internship for
+							the Fall 2023 semester so if you&apos;re interested in working
+							with me, feel free to reach out!
+						</p>
 
-            <p className=' font-medium'>
-              On another note, I&apos;m a <b>standup comedian</b> on the side, a <b>gym rat</b>, and a <b>One Piece fanatic</b>.
-            </p>
-          </div>
-          <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark p-8 bg-light '>
-            <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-3xl bg-dark ' />
-            <Image src={me} alt='me.jpg' className='w-full h-auto rounded-2xl' />
-          </div>
-          <div className='col-span-2 flex flex-col items-end justify-between'>
-            <div className='flex flex-col items-end justify-center'>
-              <span className='text-5xl inline-block  font-bold'>
-                <Count from={0} to={7} />+
-              </span>
-              <h2 className='text-xl font-medium capitalize text-dark/75'>
-                Years Coding
-              </h2>
-            </div>
+						<p className=" font-medium">
+							On another note, I&apos;m a <b>standup comedian</b> on the side, a{" "}
+							<b>gym rat</b>, and a <b>One Piece fanatic</b>.
+						</p>
+					</div>
 
-            <div className='flex flex-col items-end justify-center'>
-              <span className='text-5xl inline-block font-bold'>
-                <Count from={0} to={12} />+
-              </span>
-              <h2 className='text-xl font-medium capitalize text-dark/75'>
-                Languages and Technologies
-              </h2>
-            </div>
+					{/* Stats */}
+					<div className="md:order-3 xl:col-span-2 col-span-8 flex xl:flex-col lg:flex-row xl:items-end items-center justify-between">
+						<div className="flex flex-col items-center xl:items-end justify-center">
+							<span className="text-5xl inline-block  font-bold">
+								<Count from={0} to={7} />+
+							</span>
+							<h2 className="text-center xl:text-right text-xl font-medium capitalize text-dark/75">
+								Years Coding
+							</h2>
+						</div>
 
-            <div className='flex flex-col items-end justify-center'>
-              <span className='text-5xl inline-block font-bold'>
-                <Count from={0} to={999} />+
-              </span>
-              <h2 className='text-base font-medium capitalize text-dark/75'>
-                Years until Luffy finds the One Piece
-              </h2>
-            </div>
-            {/* 2 years of experience, 12+ project repositories, 999+ hours coding, 200+ job rejections,  episodes until Luffy finds the One piece, 356 bones ina blue whale, hours of sleep, daily pushups */}
-          </div>
-          <div className='absolute -bottom-28 '>
-            <Image src={scrolldown} alt='scrolldown.gif' className='w-8' />
-          </div>
-        </div>
+						<div className="flex flex-col  items-center xl:items-end justify-center">
+							<span className="text-5xl inline-block font-bold">
+								<Count from={0} to={12} />+
+							</span>
+							<h2 className="text-center xl:text-right text-xl font-medium capitalize text-dark/75">
+								Languages and Technologies
+							</h2>
+						</div>
 
-        <Experiences />
-      </div>
-    </main>
-  )
-}
+						<div className="flex flex-col items-center xl:items-end justify-center">
+							<span className="text-5xl inline-block font-bold">
+								<Count from={0} to={999} />+
+							</span>
+							<h2 className="text-center xl:text-right text-base font-medium capitalize text-dark/75">
+								Years until Luffy finds the One Piece
+							</h2>
+						</div>
+					</div>
+					<div className="absolute -bottom-28 ">
+						<Image src={scrolldown} alt="scrolldown.gif" className="w-8" />
+					</div>
+				</div>
 
-export default page
+				<Experiences />
+			</div>
+		</main>
+	);
+};
+
+export default page;
--- src/utils/info.js
+++ src/utils/info.js
@@ -1,134 +1,152 @@
-import ll from 'public/PreviewLens.png'
-import arnold from 'public/DALL·E 2023-06-29 16.07.19 - solar system, vibrant colors, van gogh style.png'
-import impDemo from 'public/IMPDEMO.png'
-import sitedemo from 'public/sitedemo.png'
+import ll from "public/PreviewLens.png";
+import arnold from "public/DALL·E 2023-06-29 16.07.19 - solar system, vibrant colors, van gogh style.png";
+import impDemo from "public/IMPDEMO.png";
+import sitedemo from "public/sitedemo.png";
 
 export const projects = [
-    {
-        infoPath: '',
-        title: 'Little Lens',
-        short: 'Post Images, save Images, comment, and more!\nthis is an image sharing application (sorta like Pinterest)\n',
-        description: 'Long description',
-        image: ll,
-        link: 'https://littlelensv1.netlify.app/',
-        repo: 'https://github.com/ojn03/littleLens',
-        technologies: ['React', 'Tailwind CSS', 'Sanity.io']
+	{
+		infoPath: "",
+		title: "Little Lens",
+		short:
+			"Post Images, save Images, comment, and more!\nthis is an image sharing application (sorta like Pinterest)\n",
+		description: "Long description",
+		image: ll,
+		link: "https://littlelensv1.netlify.app/",
+		repo: "https://github.com/ojn03/littleLens",
+		technologies: ["React", "Tailwind CSS", "Sanity.io"]
+	},
+	{
+		infoPath: "",
+		title: "ojn.me",
+		short: "A website about a (really cool and handsome) guy named Olivier",
+		description: "Long description",
+		image: sitedemo,
+		link: "/",
+		repo: "https://github.com/ojn03/portfolio",
+		technologies: ["Next.js", "Framer Motion", "Tailwind CSS"]
+	},
+	{
+		infoPath: "",
+		title: "Polished Pixels",
+		short:
+			"An image processor where users can import and export images in any format, and apply various filters to them.",
+		description: "Long description",
+		image: impDemo,
+		link: "",
+		repo: "",
+		technologies: ["Java", "Swing"]
+	},
+	{
+		infoPath: "",
+		title: "Arnold the Astronaut",
+		short: "A twitter bot that likes astronomy",
+		description: "Long description",
+		image: arnold,
+		link: "",
+		repo: "",
+		technologies: ["twitter API", "NASA API", "twitter4j"]
+	}
+	// {    infoPath: '',
+	//     title: 'Candid Conversations ',
+	//     short: 'a demo chat application',
+	//     description: 'Long description',
+	//     image: 'https://picsum.photos/200/300',
+	//     link: 'https://picsum.photos/200/300',
+	//     repo: '',
+	//     technologies: ['React', 'Next.js', 'Tailwind CSS']
+	// },
+	// {    infoPath: '',
+	//     title: 'Resume Scraper',
+	//     short: 'a resume parser',
+	//     description: 'Long description',
+	//     image: 'https://picsum.photos/200/300',
+	//     link: 'https://picsum.photos/200/300',
+	//     repo: '',
+	//     technologies: ['Python', 'Regex', 'Numpys', 'Pandas']
 
-    },
-    {
-        infoPath: '',
-        title: 'ojn.me',
-        short: 'A website about a (really cool and handsome) guy named Olivier',
-        description: 'Long description',
-        image: sitedemo,
-        link: '/',
-        repo: 'https://github.com/ojn03/portfolio',
-        technologies: ['Next.js', 'Framer Motion', 'Tailwind CSS']
+	// },
+	// {    infoPath: '',
+	//     title: 'bug tracker',
+	//     short: 'A project management tool',
+	//     description: 'Long description',
+	//     image: 'https://picsum.photos/200/300',
+	//     link: 'https://picsum.photos/200/300',
+	// git : '',
+	//     technologies: ['NextJs', 'Tailwind CSS', 'MongoDB', 'Express', 'Node.js', 'PostgreSQL',]
 
-    },
-    {
-        infoPath: '',
-        title: 'Polished Pixels',
-        short: 'An image processor where users can import and export images in any format, and apply various filters to them.',
-        description: 'Long description',
-        image: impDemo,
-        link: '',
-        repo: '',
-        technologies: ['Java', 'Swing']
+	// },
+	// {    infoPath: '',
+	//     title: 'path finder',
+	//     short: 'an interactive path finding visualizer',
+	//     description: 'a tool demonstrates different path finding algorithms',
+	//     image: 'https://picsum.photos/200/300',
+	//     link: 'https://picsum.photos/200/300',
+	// git : '',
+	//     technologies: ['Next.js', 'Tailwind CSS']
 
-    },
-    {    
-        infoPath: '',
-        title: 'Arnold the Astronaut',
-        short: 'A twitter bot that likes astronomy',
-        description: 'Long description',
-        image: arnold,
-        link: '',
-        repo: '',
-        technologies: ['twitter API', 'NASA API', 'twitter4j']
-
-    },
-    // {    infoPath: '',
-    //     title: 'Candid Conversations ',
-    //     short: 'a demo chat application',
-    //     description: 'Long description',
-    //     image: 'https://picsum.photos/200/300',
-    //     link: 'https://picsum.photos/200/300',
-    //     repo: '',
-    //     technologies: ['React', 'Next.js', 'Tailwind CSS']
-    // },
-    // {    infoPath: '',
-    //     title: 'Resume Scraper',
-    //     short: 'a resume parser',
-    //     description: 'Long description',
-    //     image: 'https://picsum.photos/200/300',
-    //     link: 'https://picsum.photos/200/300',
-    //     repo: '',
-    //     technologies: ['Python', 'Regex', 'Numpys', 'Pandas']
-
-    // },
-    // {    infoPath: '',
-    //     title: 'bug tracker',
-    //     short: 'A project management tool',
-    //     description: 'Long description',
-    //     image: 'https://picsum.photos/200/300',
-    //     link: 'https://picsum.photos/200/300',
-    // git : '',
-    //     technologies: ['NextJs', 'Tailwind CSS', 'MongoDB', 'Express', 'Node.js', 'PostgreSQL',]
-
-    // },
-    // {    infoPath: '',
-    //     title: 'path finder',
-    //     short: 'an interactive path finding visualizer',
-    //     description: 'a tool demonstrates different path finding algorithms',
-    //     image: 'https://picsum.photos/200/300',
-    //     link: 'https://picsum.photos/200/300',
-    // git : '',
-    //     technologies: ['Next.js', 'Tailwind CSS']
-
-    // },
-
-]
+	// },
+];
 
 export const experiences = [
-    { 
-        position: 'Discrete Structures TA',
-        company: 'Khoury College',
-        description: 'Assisted in teaching Discrete structures concepts such as combinations, graphs, and logical statements. Hosted office hours where I tutored students 1v1 and in groups. Helped grade assignments and exams, as well as review sessions for student exams. ',        
-        location: 'Boston, MA',
-        date: 'Fall - Winter 2022',
-        technologies: ['tech1', 'tech2', 'tech3'],
-        image: 'https://picsum.photos/200/300',
-        link: 'https://www.khoury.northeastern.edu/',
-        
-    },
-    {
-        position: 'Development Research Assistant',
-        company: 'Northeastern University',
-        description: 'Aided reasearch efforts by categorizing and analyzing existing data. Wrote scripts in python to automate data collection and analysis.',
-        location: 'Boston, MA',
-        date: 'Fall 2021 - Spring 2023',
-        technologies: ['tech1', 'tech2', 'tech3'],
-        image: 'https://picsum.photos/200/300',
-        link: 'https://example.com',
-    },
-    {
-        position: 'Robotics Club',
-        company: 'Jack E Singley Academy',
-        description: 'Helped design, code, and build robots capable of navigating different environments and performing actions such as shooting a ball at a target or stacking blocks. Implemented autonomous systems as well as user controlled. Led a team to compete in VEX Robotics and FIRST Robotics Competitions.',
-        location: 'Irving, TX',
-        date: '2017 - 2021',
-        link: 'https://www.irvingisd.net/SINGLEY',
-    },
-    {
-        position: 'Merit Badge Counselor',
-        company: 'Circle Ten Counci ',
-        description: 'Created interactive lessons and actvities and led younger scouts to help them earn merit badges.',
-        location: 'Grayford, TX',
-        date: 'Summer 2019 - 2021',
-        technologies: ['tech1', 'tech2', 'tech3'],
-        image: 'https://picsum.photos/200/300',
-        link: 'https://example.com',
-    },
 
-]
+    //C4C NEU
+    //GENERATE
+	//Computer Systems TA
+	{
+		position: "Resident Assistant",
+		company: "Northeastern University",
+		description:
+			"Enforced safety and well-being of 60+ residents. Planned and hosted events to build community and foster a sense of belonging. Provided resources and support to residents. ",
+		location: "Boston, MA",
+		date: "Fall 2023 - Present",
+		link: "https://example.com"
+	},
+
+	{
+		position: "NEU Summer Bridge Mentor",
+		company: "Northeastern University",
+		description:
+			"Helped lead a group of over 300 incoming freshman towards their transition into college life at NEU. Directly mentored a group of 6 menteess, teaching  about the resources available at Northeastern and how to navigate the campus.",
+		location: "Boston, MA",
+		date: "Summer 2023",
+		link: "https://admissions.northeastern.edu/academics/summer-bridge/"
+	},
+
+	{
+		position: "Discrete Structures TA",
+		company: "Khoury College",
+		description:
+			"Assisted in teaching Discrete structures concepts such as combinations, graphs, and logical statements. Hosted office hours where I tutored students 1v1 and in groups. Helped grade assignments and exams, as well as review sessions for student exams. ",
+		location: "Boston, MA",
+		date: "Fall - Winter 2022",
+		link: "https://www.khoury.northeastern.edu/"
+	},
+	{
+		position: "Development Research Assistant",
+		company: "Northeastern University",
+		description:
+			"Aided reasearch efforts by categorizing and analyzing existing data. Wrote scripts in python to automate data collection and analysis.",
+		location: "Boston, MA",
+		date: "Fall 2021 - Spring 2023",
+		link: "https://research.northeastern.edu/"
+	},
+	{
+		position: "Robotics Club",
+		company: "Jack E Singley Academy",
+		description:
+			"Helped design, code, and build robots capable of navigating different environments and performing actions such as shooting a ball at a target or stacking blocks. Implemented autonomous systems as well as user controlled. Led a team to compete in VEX Robotics and FIRST Robotics Competitions.",
+		location: "Irving, TX",
+		date: "2017 - 2021",
+		link: "https://www.irvingisd.net/SINGLEY"
+	},
+	{
+		position: "Merit Badge Counselor",
+		company: "Circle Ten Council",
+		description:
+			"Created interactive lessons and actvities and led younger scouts to help them earn merit badges.",
+		location: "Grayford, TX",
+		date: "Summer 2019 - 2021",
+		technologies: ["tech1", "tech2", "tech3"],
+		link: "https://circleten.org/posts/29098/camp-constantin"
+	}
+];
